1389e6700a7560f7fbd6084d1d9d18a90f914183
Paul Nadola Oyinga <nadolapaul@gmail.com>
2023-06-20 12:35:25 +0300

    Done

--- server/app.py
+++ server/app.py
@@ -26,9 +26,45 @@ def animal_by_id(id):
         return response
     
     response_body = f'''
-    <ul>{animal.name}, {animal.species}</ul>
+    <ul>Name: {animal.name}</ul>
+    <ul>Species: {animal.species}</ul>
+    <ul>Zookeeper: {animal.zookeeper_id}</ul>
+    <ul>Enclosure: {animal.enclosure_id}</ul>
     '''
     response = make_response(response_body, 200)
     return response
-    
 
+@app.route('/zookeeper/<int:id>')
+def zookeeper_by_id(id):
+    zookeeper = Zookeeper.query.filter(Zookeeper.id == id).first()
+    if not zookeeper:
+        response_body = '<ul>404 animal not found</ul>'
+        response = make_response(response_body, 404)
+        return response
+    
+    response_body = f'''
+    <ul>Name: {zookeeper.name}</ul>
+    <ul>Birthday: {zookeeper.birthday}</ul>
+    '''
+    
+    response = make_response(response_body, 200)
+    return response
+
+@app.route('/enclosure/<int:id>')
+def enclosure_by_id(id):
+    enclosure = Enclosure.query.filter(Enclosure.id == id).first()
+    if not enclosure:
+        response_body = '<ul>404 animal not found</ul>'
+        response = make_response(response_body, 404)
+        return response
+    
+    response_body = f'''
+    <ul>Environment: {enclosure.environment}</ul>
+    <ul>Open To Visitors: {enclosure.open_to_visitors}</ul>
+    '''
+    
+    response = make_response(response_body, 200)
+    return response
+
+if __name__ == '__main__':
+    app.run(port=5555)
--- server/models.py
+++ server/models.py
@@ -7,40 +7,12 @@ metadata = MetaData(naming_convention={
 
 db = SQLAlchemy(metadata=metadata)
 
-# class Zookeeper(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     name = db.Column(db.String(50))
-#     birthday = db.Column(db.Date)
-#     animals = db.relationship('Animal', backref='zookeeper', lazy='dynamic')
-    
-#     def __repr__(self):
-#         return f'<Zookeeper {self.name}>'
-    
-
-# class Enclosure(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     environment = db.Column(db.String(50))
-#     open_to_visitors = db.Column(db.Boolean)
-#     animals = db.relationship('Animal', backref='enclosure', lazy='dynamic')
-    
-#     def __repr__(self):
-#         return f'<Enclosure {self.environment}>'
-
-# class Animal(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     name = db.Column(db.String(50))
-#     species = db.Column(db.String(50))
-#     zookeeper_id = db.Column(db.Integer, db.ForeignKey('zookeeper.id'))
-#     enclosure_id = db.Column(db.Integer, db.ForeignKey('enclosure.id'))
-    
-#     def __repr__(self):
-#         return f'<Animal {self.name}>'
 
 class Zookeeper(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50))
     birthday = db.Column(db.Date)
-    animals = db.relationship('Animal', backref='zookeeper', lazy='dynamic')
+    animals = db.relationship('Animal', backref='zookeeper')
     
     def __repr__(self):
         return f'<Zookeeper {self.name}>'
@@ -49,7 +21,7 @@ class Enclosure(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     environment = db.Column(db.String(50))
     open_to_visitors = db.Column(db.Boolean)
-    animals = db.relationship('Animal', backref='enclosure', lazy='dynamic')
+    animals = db.relationship('Animal', backref='enclosure')
     
     def __repr__(self):
         return f'<Enclosure {self.environment}>'
